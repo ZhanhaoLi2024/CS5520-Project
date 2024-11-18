3531646b8cbfb6c9bb0eaa5643c28e04dacb4959
CodeToysRUs <li.zhanh@northeastern.edu>
2024-11-17 21:53:06 -0800

    Add static map preview for selected location

--- src/components/Location/LocationPicker.js
+++ src/components/Location/LocationPicker.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import * as Location from "expo-location";
 
 export default function LocationPicker() {
@@ -22,14 +22,20 @@ export default function LocationPicker() {
     })();
   }, []);
 
+  const getMapPreview = (latitude, longitude) => {
+    const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
+  };
+
   return (
     <View>
       {errorMsg && <Text>{errorMsg}</Text>}
       {!errorMsg && !location && <Text>Waiting for location...</Text>}
       {location && (
-        <Text>
-          Location: {location.latitude}, {location.longitude}
-        </Text>
+        <Image
+          style={{ width: "100%", height: 200 }}
+          source={{ uri: getMapPreview(location.latitude, location.longitude) }}
+        />
       )}
     </View>
   );
