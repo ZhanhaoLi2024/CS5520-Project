10fb0a3999150162eeb97081bb15f9718f2eeb87
CodeToysRUs <li.zhanh@northeastern.edu>
2024-11-17 23:12:17 -0800

    Add camera permissions logic in ImageManager

--- src/components/Image/ImageManager.js
+++ src/components/Image/ImageManager.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { generalStyles } from "../../theme/generalStyles";
 
 export default function ImageManager() {
   const [imageUri, setImageUri] = useState();
+
+  const verifyPermissions = async () => {
+    const [permissionResponse, requestPermission] =
+      ImagePicker.useCameraPermissions();
+
+    if (permissionResponse.granted) {
+      return true;
+    }
+
+    const result = await requestPermission();
+    return result.granted;
+  };
 
   return (
     <View style={generalStyles.imageContainer}>
