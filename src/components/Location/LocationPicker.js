59b4ccc1edd7764b8b792f21830585aad0987c52
CodeToysRUs <li.zhanh@northeastern.edu>
2024-11-17 21:53:40 -0800

    Add 'Pick on Map' button for navigation

--- src/components/Location/LocationPicker.js
+++ src/components/Location/LocationPicker.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import * as Location from "expo-location";
 
-export default function LocationPicker() {
+export default function LocationPicker({ onLocationPicked }) {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -16,6 +16,10 @@ export default function LocationPicker() {
 
       let currentLocation = await Location.getCurrentPositionAsync({});
       setLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      });
+      onLocationPicked({
         latitude: currentLocation.coords.latitude,
         longitude: currentLocation.coords.longitude,
       });
@@ -37,6 +41,19 @@ export default function LocationPicker() {
           source={{ uri: getMapPreview(location.latitude, location.longitude) }}
         />
       )}
+      <Pressable
+        style={{
+          backgroundColor: "blue",
+          padding: 10,
+          alignItems: "center",
+          marginTop: 10,
+        }}
+        onPress={() => {
+          /* Add navigation logic later */
+        }}
+      >
+        <Text style={{ color: "white" }}>Pick on Map</Text>
+      </Pressable>
     </View>
   );
 }
