cc6f158f53ecb44674be8d37650416ce4dddc23e
CodeToysRUs <li.zhanh@northeastern.edu>
2024-12-01 00:26:53 -0800

    Create IngredientRecipeSearch.js

---
+++ src/components/IngredientRecipeSearch.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function IngredientRecipeSearch() {
+  return (
+    <View>
+      <Text>IngredientRecipeSearch</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
